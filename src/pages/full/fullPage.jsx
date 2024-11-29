f721f42616bd46eb66b2dd74104ae8cc6bb3d137
developer2520 <berkino2099@gmail.com>
2024-11-29 21:50:35 +0500

    Fixed overflow-x

--- src/pages/full/fullPage.jsx
+++ src/pages/full/fullPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './fullPage.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from '../../components/navbar/navbar';
 import Main from '../../components/main/main';
